e57b66bfbcf060c7b288f810d66be20b3a815fa5
Henning Korsholm Rohde <dane_127@hotmail.com>
2017-06-30 23:13:27 -0700

    Add full build version

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # build
-Build is a small utility fora compile-time major.minor.micro versioning with linker hooks for git commit tree size and hash, such as "1.4.2.120.4f54bc3d".
+Build is a small utility for compile-time major.minor.micro versioning
+with additional linker hooks for git commit tree size and hash, such as
+"1.4.2.120.4f54bc3d". The benefit of the commit tree size is that it is
+unique within a branch and monotonically increasing. The hash serves to
+both identify the exact commit and prevent collisions between brances.
+
+## Usage
+
+Define the desired version and log it on startup, say:
+```
+var version = build.New(1, 4, 2)
+
+func main() {
+   log.Printf("Foo %v", version)
+   ...
+}
+```
+
+Add the following flags to `go build` and `go install`:
+```
+-ldflags "-X github.com/herohde/build.GitTree `git rev-list HEAD | wc -l` -X github.com/herohde/build.GitHash `git rev-parse --short HEAD`"
+```
+
+## License
+
+Build is released under the [MIT License](http://opensource.org/licenses/MIT).
---
+++ build.go
@@ -0,0 +1,37 @@
+// Package build contains utilities for build-time information.
+package build
+
+import "fmt"
+
+var (
+	// GitTree is to be defined by a linker argument as the result of "git rev-list HEAD | wc -l".
+	GitTree = "0"
+	// GitHash is to be defined by a linker argument as the result of "git rev-parse --short HEAD".
+	GitHash = "nohash"
+)
+
+// Version holds the full build version, such as "1.4.2.120.4f54bc3d".
+type Version struct {
+	// Major is the major version.
+	Major int
+	// Minor is the minor version.
+	Minor int
+	// Micro is the Micro version.
+	Micro int
+	// Tree is size of the git commit tree, if defined. It is expected to be
+	// a monotonically-increasing integer and be unique within a branch.
+	Tree string
+	// Hash is the short git commit hash, if defined, to avoid version
+	// collisions for branched or privately built binaries.
+	Hash string
+}
+
+func (v Version) String() string {
+	return fmt.Sprintf("%d.%d.%d.%s.%s", v.Major, v.Minor, v.Micro, v.Tree, v.Hash)
+}
+
+// New creates a build version using the global GitTree and GitHash variables,
+// which are expected to be defined externally as linker arguments.
+func New(major, minor, micro int) Version {
+	return Version{major, minor, micro, GitTree, GitHash}
+}
